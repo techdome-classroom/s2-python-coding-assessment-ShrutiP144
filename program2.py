63b06d3e36ed3075bf5850c89dcf39194c14190c
ShrutiP144 <shrutipachori144@gmail.com>
2024-10-07 14:06:21 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        # Initialize the integer result
+        total = 0
+        
+        # Traverse through the string, checking each symbol and the next one
+        for i in range(len(s)):
+            # If the current value is less than the next one, subtract it from the total (for cases like IV, IX, etc.)
+            if i < len(s) - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, just add it to the total
+                total += roman_map[s[i]]
+        
+        return total
 
 
 
